a56005a0d2a19f80682756f250105bed58ca0fbc
ShyanJMC <joaquincrespo96@gmail.com>
2018-09-30 13:24:06 -0300

    Adding source code of Bash and Grep

--- init_instalation_and_compilation.sh
+++ init_instalation_and_compilation.sh
@@ -15,10 +15,26 @@ echo "Compiling core utils..."
 cd Invierno_shell/core/coreutils-8.30/
 make
 cd src/
-ls | grep -Ev ".h|.c|.mk|blake2|dcgen" | xargs cp -t ../../
-mkdir /var/lib/invierno/core
-ls ../../../core/ | grep -v coreutils-8.30 | xargs cp -t /var/lib/invierno/core
+mkdir -p /var/lib/invierno/core
+ls | grep -Ev ".h|.c|.mk|blake2|dcgen" | xargs cp -t /var/lib/invierno/core
+
+echo "-------------"
+echo "Copying the Ivierno's Containers"
 cp -r ../../../../Images /var/lib/invierno
+
+
+echo "-------------"
+echo "Compiling BASH and Grep"
+cd ../../bas-4.4.18/
+./configure
+make
+cp bash /var/lib/invierno/core
+cd ../grep-3.1
+./configure
+make
+cp src/grep /var/lib/invierno/core
+cp src/egrep /var/lib/invierno/core
+cp src/fgrep /var/lib/invierno/core
 
 echo "-------------"
 echo "Compiling shell....."
