65179d81457fff5ebc10c52ee713fc6c4550a8d8
ShyanJMC <joaquincrespo96@gmail.com>
2018-11-05 01:42:49 -0300

    Second version of init script

--- init_instalation_and_compilation.sh
+++ init_instalation_and_compilation.sh
@@ -1,23 +1,35 @@
 ##### Script created by ShyanJMC for ProyectoInvierno.#####
 ###########################################################
+#Requeriments;
+# GCC
+# MAKE
+###########################################################
+#### This set is only and only for you can configure
+#### ProyectoInvierno inside a docker container mknod, if is 
+#### not please comment the below line
+FORCE_UNSAFE_CONFIGURE=1
+
+###########################################################
 
 WORKDIR=~/ProyectoInvierno/
 IMAGESDIR=$WORKDIR/Images
 SHELLDIR=$WORKDIR/Invierno_shell
 UTILSDIR=$WORKDIR/Invierno_shell/core/coreutils-8.30/
 BASHDIR=$WORKDIR/Invierno_shell/core/bash-4.4.18/
-GREPDIR=$WORKDOR/Invierno_shell/core/grep-3.1/
+GREPDIR=$WORKDIR/Invierno_shell/core/grep-3.1/
 
 INVDIR=/var/lib/invierno
 INVFILE=/etc/invierno
 COREDIR=$INVDIR/core
+
+GCCARGS= -Wall -Werror -lpthread
 
 #!/bin/bash
 clear
 echo "Working...."
 echo "Making configuration files..."
 
-cp Invierno_shell/inviernorc /etc/inviernorc
+cp $SHELLDIR/inviernorc /etc/inviernorc
 touch $INVFILE
 echo "BSH=0" >> $INVFILE
 echo "DCK=1" >> $INVFILE
@@ -53,7 +65,7 @@ cp src/fgrep $INVDIR/core
 echo "-------------"
 echo "Compiling shell....."
 cd $SHELLDIR
-gcc invierno_shell.c -o invierno_shell
+gcc $GCCARGS invierno_shell.c -o invierno_shell
 cp invierno_shell /usr/bin/
 
 echo
