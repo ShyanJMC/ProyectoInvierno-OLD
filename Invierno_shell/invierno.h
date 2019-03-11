71e76e4516a1914fe200ae6f420ff41df04a0001
ShyanJMC <nothing_to_show@example.com>
2019-03-11 23:19:11 +0100

    Disabled update function until I can fix a delay into the same.

--- Invierno_shell/invierno.h
+++ Invierno_shell/invierno.h
@@ -13,6 +13,36 @@ Free Software Foundation, either version 3 of the license.
 
 Developed by ShyanJMC initially at 2018.
 */
+/*
+ * Colors, for the strings.
+ */
+
+#define SNRM  "\x1B[0m"
+#define SRED  "\x1B[31m"
+#define SGRN  "\x1B[32m"
+#define SYEL  "\x1B[33m"
+#define SBLU  "\x1B[34m"
+#define SMAG  "\x1B[35m"
+#define SCYN  "\x1B[36m"
+#define SWHT  "\x1B[37m"
+
+/*
+Store for information about the program.
+1 is errror.
+0 is OK.
+Are in the order in wich the program execute.
+*/
+struct Information 
+{
+	int 	core_files,
+		environment,
+		core_invierno_bash,
+		internal_ip_address,
+		docker,
+		invierno_container,
+		updated,
+		pthreads;
+}Init1;
 
 /* The shell automatically print an error to stderr 
  * and some message to stdout. With this information
@@ -47,18 +77,19 @@ inside the Invierno's directory.
 */
 void *simple_check()
 {
+    printf("%sChecking core programs.\n",SWHT);
 	Init1.core_files = system("/var/lib/invierno/core/ls . > /dev/null");
 	if (Init1.core_files != 0)
 	{
-		fprintf(stderr,"[FAIL]\tCore failes are damaged or missing.\n");
+		fprintf(stderr,"%s[FAIL]\tCore failes are damaged or missing.\n",SRED);
 		Init1.core_files = 1;
 	}
 	else 
 	{
-		printf("[OK]\tCore files.\n");
+		printf("%s[OK]\tCore files.\n",SWHT);
 		Init1.core_files = 0;
 	}
-	return 0;
+	return;
 }
 
 /*
@@ -67,24 +98,24 @@ Check the configuration of the environment.
 
 void *environment()
 {
-	printf("Checking environment file.\n");
+	printf("%sChecking environment file.\n",SWHT);
 	Init1.environment  = system("/var/lib/invierno/core/ls /etc/invierno > /dev/null");
 	if (Init1.environment != 0)
 	{
-		fprintf(stderr,"[FAIL]\tInvierno configuration missing.\n");
+		fprintf(stderr,"%s[FAIL]\tInvierno configuration missing.\n",SRED);
 	}
-	printf("[OK]\tEnvironment file.\n");
+	printf("%s[OK]\tEnvironment file.\n",SWHT);
 	printf("Checking if Bash is enable for the user.\n");
 	Init1.core_invierno_bash = system("/var/lib/invierno/core/grep BSH=0 /etc/invierno > /dev/null");
 	if(Init1.core_invierno_bash != 0)
 	{
-	 	fprintf(stderr,"[FAIL]\tBash for user is disabled.\n");
+	 	fprintf(stderr,"%s[FAIL]\tBash for user is disabled.\n",SRED);
 	}
 	 else 
 	{
-	 	printf("[OK]\tBash enabled.\n");
+	 	printf("%s[OK]\tBash enabled.\n",SWHT);
 	}
-	return 0;
+	return;
 }
 
 /*
@@ -93,24 +124,24 @@ Manual start of docker
 void *docker_init()
 {
 	short temporal1, temporal2;
-	printf("Starting Invierno's docker.\n");
+	printf("%sStarting Invierno's docker.\n",SWHT);
 	temporal2 = system("ps aux | grep -i docker | grep -v grep");
 	if (temporal2 != 0){
 		temporal1 = system("nohup dockerd &");
 		switch(temporal1)
 		{
 			case 0:
-				printf("[OK]\tInvierno's docker started.\n");
+				printf("%s[OK]\tInvierno's docker started.\n",SWHT);
 				break;
 			default:
-				fprintf(stderr,"[FAIL]\tInvierno's docker failed to start.\n");
+				fprintf(stderr,"%s[FAIL]\tInvierno's docker failed to start.\n",SRED);
 				break;
 		}
 	}
 	else {
-		return 0; 
+		return; 
 	}
-	return 0;
+	return;
 }
 /*
 
@@ -121,19 +152,18 @@ After a successful start, check the configuration
 void *docker_internal_init ()
 {
     short temporal, temporal2;
-    printf("Enabling and starting docker.\n");
+    printf("%sEnabling and starting docker.\n",SWHT);
     temporal = system("systemctl start docker > /dev/null && systemctl enable docker > /dev/null");
-    printf("[OK]\tInvierno's docker started.\n");
     if (temporal != 0)
     {
-        fprintf(stderr,"[FAIL]\tDocker start error.\n");
+        fprintf(stderr,"%s[FAIL]\tDocker start error.\n",SRED);
         Init1.docker = 1;
     }
     else
     {
-	docker_init;
+	docker_init();
         }
-	return 0;
+	return;
 }
 
 /*
@@ -145,52 +175,52 @@ the last security updates.
 void *update()
 {
 	short buffer;
-	printf("Updating the system.\n");
+	printf("%sUpdating the system.\n",SWHT);
 	buffer = system("su -c 'yes | pacman -Syu' > /dev/null 2> /dev/null");
 	if (buffer != 0)
 	{
-		fprintf(stderr,"[FAIL]\tError to update the system.\n");
+		fprintf(stderr,"%s[FAIL]\tError to update the system.\n",SRED);
 		Init1.updated = 1;
 	}
 	else 
 	{
-		printf("\n[OK]\tSystem updated.\n");
+		printf("%s\n[OK]\tSystem updated.\n",SWHT);
 		Init1.updated = 0;
 	}
-	return 0;
+	return;
 }
 
 /* Build the images of docker */
 void docker_build(){
-	return 0;
+	return;
 }
 
 /* Check the existence of Invierno's Containers */
 void invierno_images(){
 	short	temporal1, temporal2;
 	char	temporal3;
-	printf("Checking Invierno Containers.\n");
+	printf("%sChecking Invierno Containers.\n",SWHT);
 	temporal1 = system("/var/lib/invierno/core/grep -E DCK=0 /etc/invierno");
 	if (temporal1 !=0){
-		fprintf(stderr,"[FAIL]\tInvierno's containers not enabled.\n");
-		return 1;
+		fprintf(stderr,"%s[FAIL]\tInvierno's containers not enabled.\n",SRED);
+		return;
 	}
 	else {
-		printf("[OK]\tInvierno's containers enabled. Checking existence.\n");
+		printf("%s[OK]\tInvierno's containers enabled. Checking existence.\n",SWHT);
 		temporal2 = system("docker images | /var/lib/invierno/core/grep -E 'InvPent|InvProx|InvTor'");
 		if (temporal2 !=0){
-			fprintf(stderr,"[Fail]\tInvierno's containers not detected. Do you want build them? y/n; ");
+			fprintf(stderr,"%s[Fail]\tInvierno's containers not detected. Do you want build them? y/n; ",SRED);
 			temporal3 = getchar();
 			if( temporal3 == 'y'){
 				docker_build();
 			}
 			else {
-				return 0;
+				return;
 			}
 		}
 		else {
-			printf("[OK]\tInvierno's containers detected.\n");
+			printf("%s[OK]\tInvierno's containers detected.\n",SWHT);
 		}
 	}
-	return 0;
+	return;
 }
--- Invierno_shell/invierno_shell.c
+++ Invierno_shell/invierno_shell.c
@@ -23,25 +23,8 @@ Developed by ShyanJMC initially at 2018.
  * Invierno's header.
  */
 
-#include<invierno.h>
+#include"invierno.h"
 
-/*
-Store for information about the program.
-1 is errror.
-0 is OK.
-Are in the order in wich the program execute.
-*/
-struct Information 
-{
-	int 	core_files,
-		environment,
-		core_invierno_bash,
-		internal_ip_address,
-		docker,
-		invierno_container,
-		updated,
-		pthreads;
-}Init1;
 
 /*
 Execute the functions in poxis threads and check the return of the same.
@@ -51,10 +34,10 @@ int main( int first_arg, char **second_arg)
 	pthread_t thread1, thread2, thread3, thread4, thread5;
 	int	  irthread1, irthread2, irthread3, irthread4, irthread5;
 	system("clear");
-	printf("Starting Invierno.\n");
-	printf("Bash, Grep family and Coreutils are in GPLv3. Copy of license are under ProyectoInvierno/Licenses.\n");
-	printf("Checking core programs.\n");
-   	
+	printf("%sStarting Invierno.\n",SCYN);
+	printf("%sBash, Grep family and Coreutils are in GPLv3. Copy of license are under ProyectoInvierno/Licenses.\n",SGRN);
+    printf("%s==================================================================================================\n",SWHT);
+    
 	irthread1 = pthread_create(&thread1,NULL,simple_check, NULL);
 	if (Init1.core_files != 0)
 	{
@@ -78,9 +61,10 @@ int main( int first_arg, char **second_arg)
 	pthread_join(thread1, NULL);
 	pthread_join(thread2, NULL);
 	pthread_join(thread3, NULL);
-	pthread_join(thread4, NULL);
+	/* pthread_join(thread4, NULL); */
 	
 /* Execution of bash */
+    printf("%s==================================================================================================\n",SWHT);
 	system("/var/lib/invierno/core/bash --init-file /etc/inviernorc");
 	return 0;
 }
